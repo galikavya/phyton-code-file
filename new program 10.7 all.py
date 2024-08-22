27aaa02ce275ad53d789cf4074649da7a8f06a39
galikavya <galikavya9046.sse@saveetha.com>
2024-08-22 19:56:24 +0530

    Add files via upload

---
+++ new program 10.7 all.py
@@ -0,0 +1,87 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+basic=1000
+hra(10*basic)/100
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    hra(10*basic)/100
+NameError: name 'hra' is not defined
+basic=1000
+hra(10*basic)/100
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    hra(10*basic)/100
+NameError: name 'hra' is not defined
+basic=10000
+hra=(10*basic)/100
+da=(5*basic)/100
+tot=basic+hra+da
+print(hra)
+1000.0
+print(tot)
+11500.0
+print(da)
+500.0
+print("simple calcualator")
+simple calcualator
+
+======================================== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/new program 1.1.py =======================================
+simpl calculator
+select opeeration:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+Enter choice (1/2/3/4): 1
+Enter first number: 43
+Enter second number:32
+43.0 + 32.0 = 75.0
+a=100
+b=50
+temp=a
+a=b
+b=temp
+print(a,b)
+50 100
+a=5
+b=10
+>>> a,b=b,a
+>>> print(a,b)
+10 5
+>>> def add(a,b,c):
+...     c=a+b
+...     print(c)
+...     add(10,20)
+...     add(30,60)
+...     
+... 
+... def myFun(x, y=50):
+...     print("x: ", x)
+...     print("y: ", y)
+...     
+SyntaxError: invalid syntax
+>>> 
+========================================== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/new 10 y=20.py =========================================
+x:  10
+y:  50
+>>> 
+====================================== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/key word arrugment.py ======================================
+Geeks Practice
+Geeks Practice
+>>> 
+====================================== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/position arrugmentr.py =====================================
+Case-1:
+Hi, I am Suraj
+My age is  27
+
+Case-2:
+Hi, I am 27
+My age is  Suraj
+>>> 
+======================================== RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python312/new program 1.1.py =======================================
+simpl calculator
+select opeeration:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
---
+++ number 3 file.py
@@ -0,0 +1,8 @@
+a=100
+b=51
+c=a+b
+d=a-b
+e=a/b
+f=a*b
+g=a%b
+print(c,d,e,f,g)
